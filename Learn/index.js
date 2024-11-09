9af8ec32f0eec12db380cb2da07d4471ad88ac2a
Neha k Khengar <141397029+khengarneha20@users.noreply.github.com>
2024-11-09 16:29:23 +0530

    Add files via upload

---
+++ Learn/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+//Creating a new file 
+fs.writeFileSync("read.txt" , "Welcome to my Page neha khengar" , );
+
+fs.appendFileSync("read.txt" , "This is appended filer");
